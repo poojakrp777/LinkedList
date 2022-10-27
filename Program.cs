2d1e8b6813107aa143874a533f8b7464232252a1
poojakrp777 <poojakrp777@gmail.com>
2022-10-27 22:10:20 +0530

    create a linked list

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LinkedList
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome To the Data structure program using Linked List");
+
+            Listoperation listOperation = new Listoperation();
+            listOperation.insert(56);
+            listOperation.insert(30);
+            listOperation.insert(70);
+            listOperation.display();
+        }
+    }
+}
